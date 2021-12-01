377c1d5147a980d61891f6426a4338683a56e0eb
Matt Windsor <mattwindsor91@gmail.com>
2021-12-01 21:19:33 +0000

    Initial commit; Day 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of Code 2021
+
+Solutions in C#10, maybe?
+
+## Done so far
+
+- Day 1 parts 1 and 2
+
+# License
+
+Do what the fuck you want to.  (See `LICENSE`).
---
+++ csharp/AoC2021/Day1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Solves AoC day 1 in C# 10.
+//
+// Usage:
+// put your sample in sample.txt and input in input.txt in the working
+// directory.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+
+void Run(string file)
+{
+    var readings = ReadReadings(file).ToImmutableList();
+    Console.WriteLine($"{file} part 1: {Count(readings)}");
+    Console.WriteLine($"{file} part 2: {Count(Window3(readings))}");
+}
+
+IEnumerable<int> ReadReadings(string s)
+{
+    return from line in File.ReadAllLines(s) select int.Parse(line);
+}
+
+IEnumerable<int> Window3(ImmutableList<int> input)
+{
+    var raw = input.Zip(input.Take(Range.StartAt(1))).Zip(input.Take(Range.StartAt(2)));
+    return from r in raw select r.First.First + r.First.Second + r.Second;
+}
+
+int Count(IEnumerable<int> readings)
+{
+    var last = -1;
+    var i = -1;
+
+    foreach (var r in readings)
+    {
+        if (last < r) i++;
+
+        last = r;
+    }
+
+    return i;
+}
+
+Run("sample.txt");
+Run("input.txt");
